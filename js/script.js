147388e91af541e36bcb839f076e39b216870f40
wildan agustian <wildan.agustian.production@gmail.com>
2026-01-30 07:46:50 +0700

    perubahan 1

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-// togle class active
+// togle class active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 // ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
+
+// togle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-btn").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
 // Klik di luar sidebar untuk menghilangkan nav
 const hamburger = document.querySelector("#hamburger-menu");
 document.addEventListener("click", function (e) {
