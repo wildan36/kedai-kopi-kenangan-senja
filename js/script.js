c07ccc91cef54b4b9da003a071d86760f82011ef
wildan agustian <wildan.agustian.production@gmail.com>
2026-02-06 13:51:35 +0700

    menambahkan css masking

--- js/script.js
+++ js/script.js
@@ -15,10 +15,47 @@ document.querySelector("#search-btn").onclick = (e) => {
   e.preventDefault();
 };
 
-// Klik di luar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector("#hamburger-menu");
+// togle clas active utuk shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-btn").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
+// Klik di luar elemen
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-btn");
+const sc = document.querySelector("#shopping-cart-btn");
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
 });
+
+// modal box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailBtns = document.querySelectorAll(".item-detail-btn");
+itemDetailBtns.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// klik tombol close modal
+document.querySelector(".close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+// klik di luar modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
